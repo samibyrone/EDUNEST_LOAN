dcb336b89d538d3bdcb9832393f7990c7eedde08
Samson Ibironke <samsonibironke2@gmail.com>
2025-05-17 17:22:04 +0100

    edunest application updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -94,7 +94,7 @@ export const Navbar = () => {
                     </Link>
 
                     <div className=" lg:hidden">
-                        <Button className="bg-brightColor text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out" onClick={openForm}>
+                        <Button className="bg-green text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out" onClick={openForm}>
                             Login
                         </Button>
                     </div>
